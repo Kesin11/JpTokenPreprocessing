d7adb7b1515edce9d615cb662fed09ac6bda303f
Kenta Kase <kesin1202000@gmail.com>
2015-10-12 22:52:39 +0900

    Add Python 3.2, 3,5 support description

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='JpTokenPreprocessing',
-    version='0.1.5a',
+    version='0.1.5a2',
     description='JpTokenPreprocessing is Python library for token preprocessing.',
     long_description=long_description,
     url='https://github.com/Kesin11/JpTokenPreprocessing',
@@ -29,8 +29,10 @@ setup(
         'Topic :: Text Processing',
         'Topic :: Text Processing :: Filters',
         'Natural Language :: Japanese',
+        'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ],
     keywords='NLP, natural language processing, token, japaneese',
 )
